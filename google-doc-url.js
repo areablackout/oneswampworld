1b2e12745ee46a0572cfeb9ad2631116ee9e0437
areablackout <58238843+areablackout@users.noreply.github.com>
2019-11-27 16:59:24 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UQ_pmUBGSIM7DAV4lXbIJBMlE_rPEHZXZHW5B5H3Zhc/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UQ_pmUBGSIM7DAV4lXbIJBMlE_rPEHZXZHW5B5H3Zhc/';
